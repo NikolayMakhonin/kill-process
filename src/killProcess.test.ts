4e62e850ebc16ed1fec47500f236f4e9ee587ed9
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2021-07-24 13:46:07 +0300

    feat: killProcess test completed

--- src/killProcess.test.ts
+++ src/killProcess.test.ts
@@ -72,6 +72,8 @@ describe('killProcess', function () {
 		startProc()
 		await delay(1000)
 
+		let predicateCallsCount = 0
+
 		const result = await killProcess({
 			description: 'TestDescription',
 			stages: [
@@ -82,14 +84,17 @@ describe('killProcess', function () {
 				{signal: 'SIGKILL', timeout: 1000},
 			],
 			predicate(proc, processTree, stage, stageIndex, stages) {
+				predicateCallsCount++
 				assert.ok(stage.signal !== 'SIGKILL')
 				checkPredicateArgs(proc, processTree, stage, stageIndex, stages)
 				return proc.command.indexOf(command) >= 0
 			},
 		})
 
+		assert.ok(predicateCallsCount >= 4)
+
 		assert.ok(Array.isArray(result), 'result=' + result)
-		assert.ok(result.length === 3, 'result.length !== 2: ' + JSON.stringify(result, null, 4))
+		assert.ok(result.length === 3, `result.length=${result.length}: ` + JSON.stringify(result, null, 4))
 
 		assert.strictEqual(result[0].signal, 0)
 		assert.ok(result[0].process.command.indexOf(command) >= 0)
@@ -103,21 +108,49 @@ describe('killProcess', function () {
 		assert.ok(result[2].process.command.indexOf(command) >= 0)
 		assert.ok(!result[2].error)
 	})
-	//
-	// it('timeout', async function () {
-	// 	await waitProcessTree({
-	// 		description: 'TestDescription',
-	// 		timeout: 2000,
-	// 		checkInterval: 200,
-	// 		predicate(processTree) {
-	// 			return false
-	// 		},
-	// 	})
-	// 		.then(() => {
-	// 			assert.fail('Should be error')
-	// 		})
-	// 		.catch(err => {
-	// 			assert.ok(/\bTestDescription\b/.test(err.message), err.message)
-	// 		})
-	// })
+
+	it('timeout', async function () {
+ 		const command = `setTimeout(function() { console.log('Completed') }, 30000)`
+
+		let proc
+		let error
+		function startProc() {
+			proc = spawn('node', ['-e', command])
+			proc.on('error', err => {
+				error = err
+			})
+		}
+
+		let predicateCallsCount = 0
+
+		startProc()
+		await delay(1000)
+
+		const stage = {signal: 0, timeout: 1000}
+
+		await killProcess({
+			description: 'TestDescription',
+			stages: [
+				stage,
+			],
+			predicate(proc, processTree, _stage, stageIndex, stages) {
+				predicateCallsCount++
+				assert.strictEqual(_stage, stage)
+				assert.strictEqual(_stage.signal, 0)
+				assert.strictEqual(_stage.timeout, 1000)
+				checkPredicateArgs(proc, processTree, _stage, stageIndex, stages)
+				return proc.command.indexOf(command) >= 0
+			},
+		})
+			.then(() => {
+				assert.fail('Should be error')
+			})
+			.catch(err => {
+				assert.ok(/\bTestDescription\b/.test(err.message), err.message)
+			})
+
+		assert.ok(predicateCallsCount >= 4)
+
+		process.kill(proc.pid, 'SIGTERM')
+	})
 })
