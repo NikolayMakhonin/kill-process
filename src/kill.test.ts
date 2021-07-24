a55377202631271e29e5b17f919b6485f0ba738c
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2021-07-24 22:49:09 +0300

    Debug

--- src/kill.test.ts
+++ src/kill.test.ts
@@ -15,14 +15,14 @@ describe('kill', function () {
 		let error
 		function startProc() {
 			proc = spawn('node', ['-e', command], {
-				// detached: true,
-				// stdio   : 'ignore',
+				detached: true,
+				stdio   : 'ignore',
 				windowsHide: true,
 			})
-			// proc.unref()
-			proc.on('error', err => {
-				error = err
-			})
+			proc.unref()
+			// proc.on('error', err => {
+			// 	error = err
+			// })
 		}
 
 		startProc()
