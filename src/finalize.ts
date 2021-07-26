549a64562e8f23444585816fe76c61a768cd2a4a
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2021-07-26 14:40:23 +0300

    v0.0.3

--- src/finalize.ts
+++ src/finalize.ts
@@ -7,27 +7,27 @@ import {createKillOutsideFilter} from './factories'
 
 const SOFT_KILL_DELAY_DEFAULT = 30000
 
-export async function finalizeProcess({
+export async function finalizeProcesses({
 	description,
-	soft,
 	firstDelay,
+	softKillFirst,
 	softKillDelay = SOFT_KILL_DELAY_DEFAULT,
 	outside,
-	filterArgs,
+	filters,
 }: {
 	description?: string,
-	soft?: boolean,
 	firstDelay?: number,
+	softKillFirst?: boolean,
 	softKillDelay?: number,
 	/** In separated and detached process */
 	outside?: boolean,
-	filterArgs: TProcessTreeFilterArgs
+	filters: TProcessTreeFilterArgs
 }): Promise<TKillResult[]> {
 	const stages: TKillStage[] = []
 	if (firstDelay) {
 		stages.push({timeout: firstDelay})
 	}
-	if (soft) {
+	if (softKillFirst) {
 		stages.push({signals: ['SIGHUP'], timeout: softKillDelay})
 		stages.push({signals: ['SIGINT'], timeout: softKillDelay})
 		stages.push({signals: ['SIGTERM'], timeout: softKillDelay})
@@ -35,7 +35,7 @@ export async function finalizeProcess({
 	stages.push({signals: ['SIGKILL'], timeout: softKillDelay})
 
 	if (outside) {
-		const filter = await createKillOutsideFilter(filterArgs)
+		const filter = await createKillOutsideFilter(filters)
 
 		return killManyOutside({
 			description,
@@ -43,7 +43,7 @@ export async function finalizeProcess({
 			...filter,
 		}) as any
 	} else {
-		const filter = await createProcessTreeFilter(filterArgs)
+		const filter = await createProcessTreeFilter(filters)
 
 		return killMany({
 			description,
